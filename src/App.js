e940fa0619e27dc1b87858da3fe8418946fa0b0e
Alex Bilba <alex.shanless@gmail.com>
2020-05-02 12:41:05 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CssBaseLine from "@material-ui/core/CssBaseLine";
+import Home from "./components/index";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <CssBaseLine />
+      <Home />;
+    </>
+  );
+}
+
+export default App;
