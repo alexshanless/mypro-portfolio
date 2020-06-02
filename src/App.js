d6671f7ff632fd12334afade1e4442efcb7e04ff
Alex Bilba <alex.shanless@gmail.com>
2020-06-01 18:25:20 -0700

    m

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Home from './components/index';
-import './App.css';
-import Resume from "./components/Resume"
-import Portfolio from './components/Portfolio';
-import Contacts from './components/Contacts';
-import NavBar from './components/Navbar';
+import Home from "./components/index";
+import "./App.css";
+import Resume from "./components/Resume.jsx";
+import Portfolio from "./components/Portfolio";
+import Contacts from "./components/Contacts";
 
 function App() {
-  console.log(process.env.PUBLIC_URL)
   return (
     <>
       <CssBaseline />
-      <NavBar />
-      <Route exact path={process.env.PUBLIC_URL} component={Home} />
+      <Route exact path="/" component={Home} />
       <Route path="/resume" component={Resume} />
-      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/Portfolio" component={Portfolio} />
       <Route path="/contacts" component={Contacts} />
-      {/* <Route component={Home} /> */}
     </>
-
   );
 }
 
